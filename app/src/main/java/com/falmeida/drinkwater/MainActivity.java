07e3cdb91f4964437c131ff3b33ff8385e409eac
falmeida <flaviomslima@gmail.com>
2022-02-26 07:21:11 -0300

    Initial Project Button Listen

--- app/src/main/java/com/falmeida/drinkwater/MainActivity.java
+++ app/src/main/java/com/falmeida/drinkwater/MainActivity.java
@@ -3,12 +3,40 @@ package com.falmeida.drinkwater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNotify;
+    private EditText etMinutes;
+    private TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNotify = findViewById(R.id.btnNotify);
+        etMinutes = findViewById(R.id.etNumberIterval);
+        timePicker = findViewById(R.id.timePicker);
+
+        timePicker.setIs24HourView(true);
+
+        btnNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sInterval = etMinutes.getText().toString();
+
+                int hour = timePicker.getCurrentHour();
+                int minute = timePicker.getCurrentMinute();
+                int interval = Integer.parseInt(sInterval);
+
+                Log.d("Teste", "hora: " + hour + " minute: " + minute + " interval: " + interval);
+            }
+        });
     }
 }
